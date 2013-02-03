9df21a151e7cda8d215e9b2baf1dbc5443c35a0c
Claus Due <claus@wildside.dk>
2013-02-03 23:21:18 +0100

    [TER] 1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "fluidpages_bootstrap".
  *
- * Auto generated 10-01-2013 17:54
+ * Auto generated 03-02-2013 23:21
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '1.1.1',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"5f04";s:9:"README.md";s:4:"965c";s:34:"Configuration/TypoScript/setup.txt";s:4:"7a29";s:35:"Resources/Private/Layouts/Page.html";s:4:"a0f3";s:43:"Resources/Private/Partials/PageObjects.html";s:4:"b2e4";s:44:"Resources/Private/Templates/Page/Render.html";s:4:"cf3e";s:49:"Resources/Private/Templates/Page/WithSidebar.html";s:4:"f986";s:66:"Resources/Private/Templates/ViewHelpers/Widget/Paginate/Index.html";s:4:"1309";}',
+	'_md5_values_when_last_written' => 'a:14:{s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"5f04";s:9:"README.md";s:4:"965c";s:34:"Configuration/TypoScript/setup.txt";s:4:"892e";s:40:"Resources/Private/Layouts/ErrorPage.html";s:4:"dcea";s:40:"Resources/Private/Layouts/FrontPage.html";s:4:"776f";s:35:"Resources/Private/Layouts/Page.html";s:4:"d6e1";s:43:"Resources/Private/Partials/PageObjects.html";s:4:"f412";s:43:"Resources/Private/Templates/Page/Error.html";s:4:"fe0f";s:47:"Resources/Private/Templates/Page/FrontPage.html";s:4:"9a33";s:44:"Resources/Private/Templates/Page/Render.html";s:4:"d1b4";s:49:"Resources/Private/Templates/Page/WithSidebar.html";s:4:"2bee";s:66:"Resources/Private/Templates/ViewHelpers/Widget/Paginate/Index.html";s:4:"1309";s:42:"Resources/Public/Stylesheets/Jumbotron.css";s:4:"7958";}',
 );
 
 ?>
