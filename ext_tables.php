84f32524c5865981b84c419006074c40519d7567
Claus Due <claus@wildside.dk>
2013-03-28 22:45:02 +0100

    [TASK] Register templates using PHP instead of TS

--- ext_tables.php
+++ ext_tables.php
@@ -5,3 +5,5 @@ if (!defined ('TYPO3_MODE')) {
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Fluid Pages Bootstrap: Page Templates');
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Settings', 'Fluid Pages Bootstrap: TS based settings');
+
+Tx_Fluidpages_Core::registerProviderExtensionKey('fluidpages_bootstrap');
