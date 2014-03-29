a9b9ea67a6d6983ed82bdfa8c7757621b8c73746
Claus Due <claus@namelesscoder.net>
2014-03-29 02:18:55 +0100

    [TER] 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "fluidpages_bootstrap".
  *
- * Auto generated 19-05-2013 14:37
+ * Auto generated 29-03-2014 02:18
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,9 +14,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Fluid Pages: Twitter Bootstrap Templates',
 	'description' => 'An extension providing a set of specially formatted Fluid templates usable as page templates enabled by EXT:fluidpages',
 	'category' => 'misc',
-	'author' => 'Claus Due',
+	'author' => 'FluidTYPO3 Team',
 	'author_email' => 'claus@namelesscoder.net',
-	'author_company' => 'Wildside A/S',
+	'author_company' => '',
 	'shy' => '',
 	'dependencies' => 'cms,fluidpages,vhs',
 	'conflicts' => '',
@@ -29,10 +29,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '2.0.0',
+	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.6-6.1.99',
+			'typo3' => '6.1.0-6.2.99',
 			'cms' => '',
 			'fluidpages' => '',
 			'vhs' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:20:"class.ext_update.php";s:4:"d54f";s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"4e92";s:9:"README.md";s:4:"0524";s:37:"Classes/Controller/PageController.php";s:4:"7c66";s:34:"Configuration/TypoScript/setup.txt";s:4:"6385";s:47:"Configuration/TypoScript/Settings/constants.txt";s:4:"4552";s:43:"Configuration/TypoScript/Settings/setup.txt";s:4:"9c49";s:40:"Resources/Private/Layouts/ErrorPage.html";s:4:"a568";s:40:"Resources/Private/Layouts/FrontPage.html";s:4:"e639";s:35:"Resources/Private/Layouts/Page.html";s:4:"06bc";s:43:"Resources/Private/Partials/PageObjects.html";s:4:"d6cd";s:43:"Resources/Private/Templates/Page/Error.html";s:4:"b4f6";s:47:"Resources/Private/Templates/Page/FrontPage.html";s:4:"2226";s:44:"Resources/Private/Templates/Page/Render.html";s:4:"46f5";s:49:"Resources/Private/Templates/Page/WithSidebar.html";s:4:"ce6f";s:66:"Resources/Private/Templates/ViewHelpers/Widget/Paginate/Index.html";s:4:"1309";s:36:"Resources/Public/Icons/FrontPage.png";s:4:"22a1";s:31:"Resources/Public/Icons/Page.png";s:4:"360b";s:38:"Resources/Public/Icons/SidebarPage.png";s:4:"bfc3";s:42:"Resources/Public/Stylesheets/Jumbotron.css";s:4:"7958";}',
+	'_md5_values_when_last_written' => 'a:25:{s:20:"class.ext_update.php";s:4:"d4e1";s:13:"composer.json";s:4:"2896";s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"4e92";s:9:"README.md";s:4:"d7f2";s:22:"Build/ImportSchema.sql";s:4:"4c12";s:28:"Build/LocalConfiguration.php";s:4:"c1e3";s:23:"Build/PackageStates.php";s:4:"68a1";s:37:"Classes/Controller/PageController.php";s:4:"abdd";s:34:"Configuration/TypoScript/setup.txt";s:4:"6385";s:47:"Configuration/TypoScript/Settings/constants.txt";s:4:"4552";s:43:"Configuration/TypoScript/Settings/setup.txt";s:4:"9c49";s:40:"Resources/Private/Layouts/ErrorPage.html";s:4:"6e89";s:40:"Resources/Private/Layouts/FrontPage.html";s:4:"3d23";s:35:"Resources/Private/Layouts/Page.html";s:4:"a3ce";s:43:"Resources/Private/Partials/PageObjects.html";s:4:"9b48";s:43:"Resources/Private/Templates/Page/Error.html";s:4:"1d66";s:47:"Resources/Private/Templates/Page/FrontPage.html";s:4:"ba81";s:44:"Resources/Private/Templates/Page/Render.html";s:4:"ff4f";s:49:"Resources/Private/Templates/Page/WithSidebar.html";s:4:"fb03";s:66:"Resources/Private/Templates/ViewHelpers/Widget/Paginate/Index.html";s:4:"1309";s:36:"Resources/Public/Icons/FrontPage.png";s:4:"22a1";s:31:"Resources/Public/Icons/Page.png";s:4:"360b";s:38:"Resources/Public/Icons/SidebarPage.png";s:4:"bfc3";s:42:"Resources/Public/Stylesheets/Jumbotron.css";s:4:"7958";}',
 );
 
 ?>
